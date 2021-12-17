d165a6afb155d6cda99070acd8327d822202c72b
Kirill <kirill.artukhov@bk.ru>
2021-12-17 03:03:45 +0300

    new

---
+++ алфавит/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+void fun(int n, char string[100]);
+
+int main()
+{
+    char string[100];
+    int n;
+
+    cout << "String: ";
+    gets_s(string);
+    n = strlen(string);
+    fun(n, string);
+    cout << string;
+}
+
+void fun(int n, char string[100])
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (string[i] > string[j])
+            {
+                char f;
+                f = string[i];
+                string[i] = string[j];
+                string[j] = f;
+            }
+        }
+    }
+}
---
+++ максимальные абсолютные величины на главной диагонали/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+void zapolnenie(int i, int j, int n, int** arr);
+void vivod(int i, int j, int n, int** arr);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+
+    int n, k = 0, i = 0, j = 0;
+
+    cout << "Введите размер квадратной матрицы: ";
+    cin >> n;
+    int** arr = new int* [n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i] = new int[n];
+        }
+    }
+
+    zapolnenie(i, j, n, arr);
+    vivod(i, j, n, arr);
+
+    for (int j = 0; j < n; j++)
+    {
+        int max = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (fabs(arr[i][j]) > fabs(arr[max][j]))
+                max = i;
+            for (int k = 0; k < n; k++)
+            {
+                int f = arr[max][k];
+                arr[max][k] = arr[j][k];
+                arr[j][k] = f;
+            }
+        }
+    }
+    cout << endl;
+
+    vivod(i, j, n, arr);
+
+}
+
+void vivod(int i, int j, int n, int **arr) {
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void zapolnenie(int i, int j, int n, int** arr)
+{    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+}
